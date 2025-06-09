a9728290e35ee1174763b5f664d23543f51639aa
Alendroide <alendroideyt@gmail.com>
2025-06-09 09:52:59 -0500

    pet lat and lng added to pet creation

--- scripts/create.js
+++ scripts/create.js
@@ -1,24 +1,46 @@
 window.submitForm = submitForm;
 
+// LEAFLET
+    // Create the map and set a default view
+    const map = L.map('map').setView([1.851740, -76.046878], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
+
+    let coord = undefined;
+    let marker;
+
+    // Handle map click
+    map.on('click', function (e) {
+        const { lat, lng } = e.latlng;
+        if (marker) {
+            marker.setLatLng([lat, lng]);
+            } else {
+            marker = L.marker([lat, lng]).addTo(map);
+        }
+        coord = {lat,lng};
+    });
+
 async function submitForm(){
 
     // Limpiar error
     document.getElementById("name.error").innerHTML = ""
     document.getElementById("race_id.error").innerHTML = ""
     document.getElementById("gender_id.error").innerHTML = ""
+    document.getElementById("coord_id.error").innerHTML = ""
 
     // Obtener datos
     const name = document.getElementById("name").value;
     const race_id = document.getElementById("race_id").value;
     const gender_id = document.getElementById("gender_id").value;
     const photo = document.getElementById("photo").files?.[0];
-    console.log(photo)
 
     // Checkear errores
     if(!name) document.getElementById("name.error").innerHTML = "Ingrese un nombre"
     if(!race_id) document.getElementById("race_id.error").innerHTML = "Ingrese una raza"
     if(!gender_id) document.getElementById("gender_id.error").innerHTML = "Ingrese un género"
-    if(!name || !race_id || !gender_id) return;
+    if(!coord) document.getElementById("coord_id.error").innerHTML = "Seleccione una posición"
+    if(!name || !race_id || !gender_id || !coord) return;
 
     const formData = new FormData();
 
@@ -26,6 +48,8 @@ async function submitForm(){
     formData.append("race_id",race_id);
     formData.append("gender_id",gender_id);
     formData.append("photo",photo);
+    formData.append("lat",coord.lat);
+    formData.append("lng",coord.lng);
 
     const response = await fetch(`${API_URL}/pets/create`,{
         body : formData,
